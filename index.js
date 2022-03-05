e90c7a0842a522968fbc5733e276f1040b142d63
khali70 <k7alifa.a7med70@gmail.com>
2022-03-05 16:57:20 +0200

    working model

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const tf = require('@tensorflow/tfjs-node');
+const app = express();
+(async () => {
+
+  const model = await tf.loadLayersModel('file://' + './model.json')
+  app.use(express.json())
+  app.post('/', async (req, res) => {
+    const data = req.body.data?.split(',').map(parseFloat);
+    const shape = req.body?.shape || [1, 5]
+    const p = model.predict(tf.tensor2d(data, shape))
+    const value = Array.from(p.dataSync());
+    const result = value.indexOf(Math.max(...value))
+    if (result > -1) {
+      res.send(JSON.stringify({ res: result }))
+    } else {
+      res.send(JSON.stringify({ error: "invalid encoding" }))
+    }
+
+  });
+
+  app.listen(process.env.PORT || 3000);
+  console.log('Web Server is listening at PORT ' + (process.env.PORT || 3000));
+})()
