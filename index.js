7ea251bfc7b194ff16b6d5f8f8d1fba371e0e12e
khali70 <k7alifa.a7med70@gmail.com>
2022-08-01 21:21:49 +0200

    update routes

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const express = require('express');
-const tf = require('@tensorflow/tfjs-node');
+const { initializeApp } = require('@firebase/app');
+const { getFirestore, getDocs, collection, doc } = require('@firebase/firestore');
+const { firebaseConfig } = require('./firebase.config');
+const { getRemoteFile } = require('./download')
+
 const app = express();
 (async () => {
 
-  const model = await tf.loadLayersModel('file://' + './model.json')
   app.use(express.json())
+  app.use(express.static('public'))
   app.get('/', async (req, res) => {
     res.send('Hello world')
   })
-  app.post('/', async (req, res) => {
-    const data = req.body.data?.split(',').map(parseFloat);
-    const shape = req.body?.shape || [1, 5]
-    const p = model.predict(tf.tensor2d(data, shape))
-    const value = Array.from(p.dataSync());
-    const result = value.indexOf(Math.max(...value))
-    res.setHeader('Content-Type', 'application/json');
-    if (result > -1) {
-      res.send(JSON.stringify({ res: result }))
-    } else {
-      res.send(JSON.stringify({ error: "invalid encoding" }))
+  app.post('/update', async (req, res) => {
+    try {
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+
+      const querySnapshot = await getDocs(collection(db, "/LifeSign/SW/ai"));
+      const docs/* : AI[] */ = [];
+      (querySnapshot /* as QuerySnapshot<AI> */).forEach((doc) => {
+        docs.push(doc.data());
+      });
+      if (!docs?.[0]) {
+        throw new Error(`model object not found`);
+      }
+      const ai = docs[0];
+      getRemoteFile(`public/` + ai.model.title, ai.model.src);
+      getRemoteFile(`public/` + ai.weights.title, ai.weights.src);
+      res.status(200);
+      res.send({ body: 'updated' })
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+      res.send({ body: 'error while updating' })
     }
 
-  });
-
+  })
   app.listen(process.env.PORT || 3000);
   console.log('Web Server is listening at PORT ' + (process.env.PORT || 3000));
 })()
